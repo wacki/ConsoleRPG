5e84a0de9cef69e216af5441b4b3f72c0f0d8957
Colin Ross <gd45colin@vfs.local>
2016-10-05 20:15:09 -0700

    added monster class

---
+++ ConsoleRPG/Monsters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG
+{
+    public class Monsters
+    {
+        string m_sName;
+        int m_iHealth;
+        int m_iAttackBase;
+
+        public Monsters() 
+            {
+
+            }
+
+        public void Identify()
+        {
+            string[] MonsterNames = new string[6];
+            MonsterNames[0] = "Goblin";
+            MonsterNames[1] = "Orc";
+            MonsterNames[2] = "Wolf";
+            MonsterNames[3] = "Giant Spider";
+            MonsterNames[4] = "Lizardfolk";
+            MonsterNames[5] = "Alligator";
+
+            Console.WriteLine("You are fighting a {0}", MonsterNames[0]);
+        }
+
+        //Goblin stats
+
+    }
+}
